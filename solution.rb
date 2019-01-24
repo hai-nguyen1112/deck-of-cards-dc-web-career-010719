4694f6ab42f4ab4ad91c72da47ace65a1201c8ec
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-01-24 13:12:42 -0500

    done

--- solution.rb
+++ solution.rb
@@ -1,7 +1,33 @@
+require 'pry'
+
 class Deck
+
+  attr_reader :cards
+
+  def initialize
+    @ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    @suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    @cards = []
+      @ranks.each do |rank|
+        @suits.each do |suit|
+          @cards << Card.new(suit, rank)
+        end
+      end
+  end
+
+  def choose_card
+    self.cards.shuffle!.pop
+  end
 
 end
 
 class Card
+
+  attr_reader :rank, :suit
+
+  def initialize(suit, rank)
+    @rank = rank
+    @suit = suit
+  end
 
 end
